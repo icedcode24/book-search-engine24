b5bd9d94c5a4c03411b6a334a7713e86c1165481
Cedric <cedricjhamilton@gmail.com>
2024-04-22 19:52:07 -0400

    updated server js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,19 +5,20 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
-    }
+    // if (!token) {
+    //   return res.status(400).json({ message: 'You have no token!' });
+    // }
 
     // verify token and get user data out of it
     try {
@@ -25,11 +26,11 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      return req;
     }
 
     // send to next endpoint
-    next();
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
